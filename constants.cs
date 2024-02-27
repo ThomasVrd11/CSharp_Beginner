8cc6db5f1c59faf9373aa223078a2999ed4e68c8
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-27 15:51:29 +0100

    constants

---
+++ constants.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // * Constants are immutable values which are known at compile time and do not change for the life of the program.
+        // * Constants are declared with the const keyword.
+        // * Constants are always implicitly static.
+
+        double weight = 78.5;       // * can change
+        const double pi = 3.14159;  // * cannot change
+
+
+    }
+}
