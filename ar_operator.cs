b5ff0987873dacd5a16d67453289f8e637dc5460
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-28 15:45:55 +0100

    operators

---
+++ ar_operator.cs
@@ -0,0 +1,45 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int friends = 5;
+        System.Console.WriteLine(friends);
+        // * input 5
+
+        friends = friends + 1;
+        System.Console.WriteLine(friends);
+        // * input 6
+
+        System.Console.WriteLine(friends + 5);
+        // * input 11
+
+        friends -= 3;
+        System.Console.WriteLine(friends);
+        // * input 3
+
+        friends *= 2;
+        System.Console.WriteLine(friends);
+        // * input 6
+
+        friends /= 2;
+        System.Console.WriteLine(friends);
+        // * input 3
+
+        friends = 10
+        friends %= 3;
+        System.Console.WriteLine(friends);
+        // * input 1
+
+        
+
+
+
+
+        // * in a loop, we can do friends++ or friends += 2 to increment the value of friends by 1 or 2 for example 
+
+
+    }
+}
